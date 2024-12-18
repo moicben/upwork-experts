55704449dde9427f8c5118176c4432f2c09e69c2
moicben <benjamain.georges@gmail.com>
2024-12-18 12:58:44 +0700

    heroku9

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
 app.use(express.json());
 
 // Gérer les requêtes préliminaires (OPTIONS)
-// app.options('*', cors());
+app.options('*', cors());
 
 /*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
